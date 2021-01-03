9b376599e81f286de9ae96cc5a2a4837b0cb3209
crisgarm <crisgarm9@gmail.com>
2021-01-03 11:23:55 +0100

    Fix html error, declare variables and define words

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const wordEl = document.querySelector(".js-word");
+const wrongLettersEl = document.querySelector(".js-wrong-letters");
+const playAgainBtn = document.querySelector(".js-play-button");
+const popup = document.querySelector(".js-popup-container");
+const notification = document.querySelector("js-notification-container");
+const finalMessage = document.querySelector("js-final-message");
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = [
+  "developer",
+  "application",
+  "programming",
+  "interface",
+  "javascript",
+  "vue",
+  "api",
+  "attribute",
+  "framework",
+  "favicon",
+  "react",
+  "angular",
+  "responsive design",
+  "accessibility",
+  "asynchronous",
+  "arrow function",
+];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+console.log(selectedWord);
+const correctLetters = [];
+const wrongLetters = [];
